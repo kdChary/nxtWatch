699135031a9d810ae8fb88050fa8f2dc37575823
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-03-25 16:55:46 +0000

    Header component completed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,42 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import {withRouter} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
 import Popup from 'reactjs-popup'
 
-import {RiCloseFill, RiMenuFill} from 'react-icons/ri'
+import {RiCloseFill, RiMenuFill, RiMoonFill, RiSunFill} from 'react-icons/ri'
+import {FiLogOut} from 'react-icons/fi'
 
 import AppContext from '../../context/AppContext'
 import MenuList from '../MenuList'
-import {Navbar, AppLogo, PopupMenu, PopupBtn} from './style'
+import {
+  Navbar,
+  AppLogo,
+  NavOptionsList,
+  NavItem,
+  NavOptionsBtn,
+  PopupMenu,
+  PopupBtn,
+  ConfirmLogoutText,
+  AlignPopupBtns,
+  LogoutBtn,
+  NavLogoutBtn,
+} from './style'
 
-const Header = () => {
+const Header = props => {
+  const logOut = () => {
+    Cookies.remove('jwt_token')
+
+    const {history} = props
+    history.replace('/login')
+  }
+
   const renderPopupMenu = isDark => (
     <Popup
       modal
       trigger={
-        <button type="button">
-          <RiMenuFill />
-        </button>
+        <NavOptionsBtn type="button" dark={isDark} mn>
+          <RiMenuFill size="23" />
+        </NavOptionsBtn>
       }
     >
       {close => (
@@ -28,25 +49,108 @@ const Header = () => {
       )}
     </Popup>
   )
-  const renderNavbar = isDark => (
-    <Navbar>
-      <AppLogo
-        src={
-          isDark
-            ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png'
-            : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png'
-        }
-        alt="website logo"
-      />
-      <div>{renderPopupMenu(isDark)}</div>
+
+  const renderLogOut = isDark => (
+    <Popup
+      modal
+      trigger={
+        <NavOptionsBtn dark={isDark} type="button" mn>
+          <FiLogOut size="23" />
+        </NavOptionsBtn>
+      }
+    >
+      {close => (
+        <PopupMenu dark={isDark} logOut>
+          <ConfirmLogoutText dark={isDark}>
+            Are you sure you want to logout?
+          </ConfirmLogoutText>
+          <AlignPopupBtns>
+            <LogoutBtn type="button" onClick={close} outline>
+              Cancel
+            </LogoutBtn>
+            <LogoutBtn type="button" onClick={logOut}>
+              Confirm
+            </LogoutBtn>
+          </AlignPopupBtns>
+        </PopupMenu>
+      )}
+    </Popup>
+  )
+
+  const renderLogOutLg = isDark => (
+    <Popup
+      modal
+      trigger={
+        <NavLogoutBtn dark={isDark} type="button" onClick={logOut}>
+          Logout
+        </NavLogoutBtn>
+      }
+    >
+      {close => (
+        <PopupMenu dark={isDark} logOut>
+          <ConfirmLogoutText dark={isDark}>
+            Are you sure you want to logout?
+          </ConfirmLogoutText>
+          <AlignPopupBtns>
+            <LogoutBtn type="button" onClick={close} outline>
+              Cancel
+            </LogoutBtn>
+            <LogoutBtn type="button" onClick={logOut}>
+              Confirm
+            </LogoutBtn>
+          </AlignPopupBtns>
+        </PopupMenu>
+      )}
+    </Popup>
+  )
+
+  const renderNavbar = (isDark, toggleTheme) => (
+    <Navbar dark={isDark}>
+      <Link to="/">
+        <AppLogo
+          src={
+            isDark
+              ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png'
+              : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png'
+          }
+          alt="website logo"
+        />
+      </Link>
+      <NavOptionsList dark={isDark}>
+        <NavItem>
+          {isDark ? (
+            <NavOptionsBtn dark={isDark} onClick={toggleTheme}>
+              <RiSunFill size="23" />
+            </NavOptionsBtn>
+          ) : (
+            <NavOptionsBtn dark={isDark} onClick={toggleTheme}>
+              <RiMoonFill size="23" />
+            </NavOptionsBtn>
+          )}
+        </NavItem>
+
+        <NavItem lg>{renderPopupMenu(isDark)}</NavItem>
+
+        <NavItem>
+          <AppLogo
+            dp
+            hide
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-profile-img.png"
+            alt="profile"
+          />
+        </NavItem>
+
+        <NavItem lg>{renderLogOut(isDark)}</NavItem>
+        <NavItem sm>{renderLogOutLg(isDark)}</NavItem>
+      </NavOptionsList>
     </Navbar>
   )
 
   return (
     <AppContext.Consumer>
       {value => {
-        const {isDark} = value
-        return <>{renderNavbar(isDark)}</>
+        const {isDark, toggleTheme} = value
+        return <>{renderNavbar(isDark, toggleTheme)}</>
       }}
     </AppContext.Consumer>
   )
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,30 +1,74 @@
 import styled from 'styled-components'
 
-/*  Styling navbar   */
+/*  Styling navbar and Logo.  */
 export const Navbar = styled.nav`
   width: 100%;
   height: 10vh;
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
+  padding: 5px 13px;
   background-color: ${props => (props.dark ? '#212121' : '#ffffff')};
   border: none;
   border-bottom: ${props => !props.dark && 'solid 2.2px #94a3b8'};
 `
 export const AppLogo = styled.img`
-  width: 96px;
+  width: ${props => (props.dp ? '25px' : '96px')};
+  display: ${props => props.dp && 'none'};
+
   @media screen and (min-width: 425px) and (max-width: 767px) {
-    width: 125px;
+    width: ${props => (props.dp ? '25px' : '125px')};
   }
+
   @media screen and (min-width: 768px) {
-    width: 135px;
+    width: ${props => (props.dp ? '25px' : '135px')};
+    display: ${props => props.dp && 'block'};
   }
+`
+
+/*  Styling Options  */
+export const NavOptionsList = styled.ul`
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 125px;
+  @media screen and (min-width: 768px) {
+    width: 245px;
+  }
+  //   border: solid 1px #cdcdcd;
+`
+export const NavItem = styled.li`
+  list-style-type: none;
+  display: block;
+
+  @media (min-width: 768px) {
+    display: ${props => props.lg && 'none'};
+  }
+  @media (max-width: 767px) {
+    display: ${props => props.sm && 'none'};
+  }
+`
+
+export const NavOptionsBtn = styled.button`
+  display: inline-block;
+  background-color: transparent;
+  border: none;
+  font-size: 19px;
+  color: ${props => (props.dark ? '#ffffff' : '#231f20')};
+  transition: 1s ease-in-out;
+  &:hover {
+    rotate: ${props => (props.mn ? '90deg' : '45deg')};
+    color: #ff000f;
+  }
+  cursor: pointer;
 `
 
 /*   Styling PopUp Section   */
 export const PopupMenu = styled.div`
-  width: 215px;
-  height: 240px;
+  width: ${props => (props.logOut ? '290px' : '240px')};
+  height: ${props => (props.logOut ? '140px' : '240px')};
 
   display: flex;
   flex-direction: column;
@@ -47,7 +91,56 @@ export const PopupBtn = styled.button`
   font-size: 19px;
   color: ${props => (props.dark ? '#ffffff' : '#231f20')};
   &:hover&:active {
-    rotate: y -45deg;
+    rotate: y ${props => (props.mn ? '90deg' : '45deg')};
     color: #ff000f;
   }
+  cursor: pointer;
+`
+export const ConfirmLogoutText = styled.p`
+  font-family: 'Roboto';
+  font-size: 15px;
+  text-align: center;
+  line-height: 1.5;
+  letter-spacing: 0.7px;
+  color: ${props => (props.dark ? '#ffffff' : '#1e293b')};
+`
+
+export const LogoutBtn = styled.button`
+  width: 96px;
+  border: solid 2px ${props => (props.outline ? '#94a3b8' : '#4f46e5')};
+  border-radius: 3px;
+  background-color: ${props => (props.outline ? 'transparent' : '#4f46e5')};
+  color: ${props => (props.outline ? '#94a3b8' : '#ffffff')};
+
+  font-weight: 500;
+  font-size: 15px;
+  font-family: 'Roboto';
+  padding: 3.6px 9px;
+  cursor: pointer;
+`
+export const AlignPopupBtns = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+`
+export const NavLogoutBtn = styled.button`
+  border: solid 2px ${props => (props.dark ? '#ffffff' : '#3b82f6')};
+  border-radius: 3px;
+  color: ${props => (props.dark ? '#ffffff' : '#3b82f6')};
+  background-color: transparent;
+  padding: 3px 9.5px;
+
+  font-family: 'Roboto';
+  font-size: 13px;
+  font-weight: 600;
+
+  width: 63px;
+  transition: 1s ease-in-out;
+  &:hover {
+    // rotate: y -45deg;
+    // color: #ff000f;
+    border: dashed 2px ${props => (props.dark ? '#ffffff' : '#3b82f6')};
+    border-style: ;
+  }
+  cursor: pointer;
 `
